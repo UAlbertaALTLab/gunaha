fe216752ee18a3a252ffbe91a4c8e0f1f9b38e15
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-25 11:02:21 -0600

    feat: Gúnahà: Danit'ada!

---
+++ apps/gunaha/urls.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from django.urls import path
+
+from . import views
+
+app_name = "gunaha"
+urlpatterns = [
+    path("", views.index, name="index"),
+]
