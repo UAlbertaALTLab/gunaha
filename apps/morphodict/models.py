80f12973f7e1641b3e2bcf6b4881a5f568327ede
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-26 10:40:41 -0600

    feat: create initial models

--- apps/morphodict/models.py
+++ apps/morphodict/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+MAX_HEAD_LENGTH = 64
+MAX_DEFINITION_LENGTH = 256
+
+
+class Head(models.Model):
+    """
+    Any linguistic utterance that is defined. This can be:
+
+        - a phrase
+        - a morpheme
+        - a wordform
+            - a wordform that is a lemma
+    """
+
+    text = models.CharField(max_length=MAX_HEAD_LENGTH)
+
+
+class Definition(models.Model):
+    """
+    One of several possible meanings for the head.
+    """
+
+    text = models.CharField(max_length=MAX_DEFINITION_LENGTH)
+    defines = models.ForeignKey(
+        Head, on_delete=models.CASCADE, related_name="definitions"
+    )
