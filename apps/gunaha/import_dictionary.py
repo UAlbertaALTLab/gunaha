5b18e7eae93fd5530b723c5ab071a21b393d781e
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-26 15:04:44 -0600

    Import za worddos

--- apps/gunaha/import_dictionary.py
+++ apps/gunaha/import_dictionary.py
@@ -6,10 +6,12 @@ import io
 import logging
 import re
 from collections import defaultdict
-from hashlib import sha384
+from hashlib import sha1, sha384
 from pathlib import Path
 from typing import Dict, Set
 from unicodedata import normalize
+
+from django.db import transaction
 
 from apps.morphodict.models import Definition, DictionarySource, Head
 
@@ -35,6 +37,10 @@ def import_dictionary() -> None:
     logger.info("Importing %s [SHA-384: %s]", path_to_tsv, file_hash)
 
     # TODO: check if already imported
+    ds = DictionarySource.objects.get(abbrv="Starlight")
+    if ds.last_import_sha384 == file_hash:
+        logger.info("Already imported %s; skipping...", path_to_tsv)
+        return
 
     starlight = DictionarySource(
         abbrv="Starlight",
@@ -50,34 +56,58 @@ def import_dictionary() -> None:
         last_import_sha384=file_hash,
     )
 
+    heads: Dict[int, Head] = {}
+    definitions: Dict[int, Definition] = {}
+
+    starlight_definition: Set[int] = set()
+    sapir_defintion: Set[int] = set()
+
     entries = csv.DictReader(tsv_file, delimiter="\t")
-    terms: Dict[int, Head] = {}
-    definitions: Set[Definition] = set()
-    pat = re.compile(r"""^os(\d+)([abcdefgh])?$""")
     for entry in entries:
         folio_id = entry["ID"]
 
-        # deal with duplicate ids...
-        match = pat.match(folio_id)
-        assert match is not None
-
-        os_id = int(match.group(1), base=10)
-        if sub_id := match.group(2):
-            sub_part = 1 + ord(sub_id) - ord("a")
-        else:
-            sub_part = 0
-
-        primary_key = os_id * 100 + sub_part
-
-        term = normalize("NFC", entry["Bruce - Tsuut'ina text"])
+        term = nfc(entry["Bruce - Tsuut'ina text"])
         word_class = entry["Part of speech"]
-        starlight_def = entry["Bruce - English text"]
-        head = terms.setdefault(
+
+        primary_key = make_primary_key(term, word_class)
+        head = heads.setdefault(
             primary_key, Head(pk=primary_key, text=term, word_class=word_class)
         )
-        if head.text and term:
-            assert head.text == term, f"mismatch: {head} / {term}"
-        if not head.word_class:
-            head.word_class = word_class
-        elif head.word_class and word_class:
-            assert head.word_class == word_class, f"mismatch: {head} / {word_class}"
+
+        starlight_def = nfc(entry["Bruce - English text"])
+        if starlight_def:
+            pk = make_primary_key(starlight_def, str(head.pk))
+            dfn = Definition(pk=pk, text=starlight_def, defines=head)
+            starlight_definition.add(pk)
+            definitions[pk] = dfn
+
+        sapir_def = nfc(entry["Sapir - English transcription"])
+        if sapir_def:
+            pk = make_primary_key(sapir_def, str(head.pk))
+            dfn = Definition(pk=pk, text=sapir_def, defines=head)
+            sapir_defintion.add(pk)
+            definitions[pk] = dfn
+
+    logger.info(
+        "will insert: heads: %d, defs: %d [starlight: %d] [sapir: %d]",
+        len(heads),
+        len(definitions),
+        len(starlight_definition),
+        len(sapir_defintion),
+    )
+
+    with transaction.atomic():
+        DictionarySource.objects.bulk_create([starlight, sapir])
+        Head.objects.bulk_create(heads.values())
+        Definition.objects.bulk_create(definitions.values())
+
+    logger.info("Done importing from %s", path_to_tsv)
+
+
+def nfc(text: str) -> str:
+    return normalize("NFC", text)
+
+
+def make_primary_key(*args: str) -> int:
+    number = int(sha1("\n".join(args).encode("UTF-8")).hexdigest(), base=16)
+    return number & 0xFFFFFFFF
