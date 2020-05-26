36a486bdb875b73582129881c1fc7d67dc6c00e4
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-26 16:01:49 -0600

    Implement one page that shows it ALL

--- apps/gunaha/views.py
+++ apps/gunaha/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from apps.morphodict.models import Head
+
 
 def index(request):
-    return render(request, "gunaha/index.html")
+    terms = Head.objects.all()
+    return render(request, "gunaha/index.html", context={"terms": terms})
