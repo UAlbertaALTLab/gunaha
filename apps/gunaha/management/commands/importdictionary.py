698dbe8eaab4702a4bf8f01faa1078e2fbc650c0
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-30 10:46:41 -0600

    Implement help for importdictionary command

--- apps/gunaha/management/commands/importdictionary.py
+++ apps/gunaha/management/commands/importdictionary.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+"""
+manage.py importdictionary
+"""
+
 from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
+    help = "Imports the Onespot-Sapir vocabulary list"
+
     def handle(self, *args, **options) -> None:
         from apps.gunaha.import_dictionary import import_dictionary
 
