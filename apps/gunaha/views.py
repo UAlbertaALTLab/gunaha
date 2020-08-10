8557d122d96344bb272eeca58fe8b7aa989826f7
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-10 16:09:29 -0600

    feat(gunaha): add basic pagination

--- apps/gunaha/views.py
+++ apps/gunaha/views.py
@@ -1,12 +1,21 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from apps.morphodict.models import Head
 
+MAX_RESULTS_PER_PAGE = 30
+
 
 def index(request):
-    terms = search_entries()[:100]
-    return render(request, "gunaha/index.html", context={"terms": terms})
+    terms = search_entries()
+    pages = Paginator(terms, MAX_RESULTS_PER_PAGE)
+    # page page get page page get get get page get
+    page = pages.get_page(request.GET.get("page", 1))
+    return render(request, "gunaha/index.html", context={"page": page},)
 
 
 def search_entries():
+    """
+    Results are ALWAYS ordered!
+    """
     return Head.objects.all().prefetch_related("definitions")
