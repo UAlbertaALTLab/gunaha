28e206d4ea315ffd69c41c3f4aefcc669a92fffb
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-11 14:19:41 -0600

    feat: store definitions

--- apps/morphodict/search_indexes.py
+++ apps/morphodict/search_indexes.py
@@ -13,9 +13,16 @@ from .models import Definition, Head
 class HeadIndex(indexes.SearchIndex, indexes.Indexable):
     # See: ./apps/morphodict/templates/search/indexes/morphodict/head_text.txt
     text = indexes.CharField(document=True, use_template=True)
+    definitions = indexes.MultiValueField(indexed=False)
 
     def get_model(self):
         return Head
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all().prefetch_related("definitions")
+
+    def prepare_definitions(self, head: Head):
+        """
+        Store the raw definition text in the database.
+        """
+        return [dfn.text for dfn in head.definitions.all()]
