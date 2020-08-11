5a544beba288964f4de6a573336fe61e380fa856
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-11 15:49:25 -0600

    change: handle empty query

--- apps/gunaha/views.py
+++ apps/gunaha/views.py
@@ -2,8 +2,7 @@ from typing import Optional
 
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from haystack.inputs import AutoQuery  # type: ignore
-from haystack.query import SearchQuerySet  # type: ignore
+from haystack.query import EmptySearchQuerySet, SearchQuerySet  # type: ignore
 
 from apps.morphodict.models import Head
 
@@ -13,11 +12,16 @@ MAX_RESULTS_PER_PAGE = 30
 
 
 def index(request):
-    results = search_entries(request.GET.get("q", None))
+    query = request.GET.get("q", None)
+    results = search_entries(query)
     pages = Paginator(results, MAX_RESULTS_PER_PAGE)
     # page page get page page get get get page get
     page = pages.get_page(request.GET.get("page", 1))
-    return render(request, "gunaha/index.html", context={"page": page},)
+    return render(
+        request,
+        "gunaha/index.html",
+        context={"page": page, "query": query, "paginator": pages},
+    )
 
 
 def search_entries(query: Optional[str]):
@@ -25,11 +29,12 @@ def search_entries(query: Optional[str]):
     Results are ALWAYS ordered!
     """
 
-    query_set = SearchQuerySet().models(Head)
-
     if query is None:
-        return query_set
+        query_set = EmptySearchQuerySet()
+    else:
+        query_set = SearchQuerySet().models(Head)
 
-    return query_set.filter(content=AutoQuery(query)) | query_set.filter(
-        head__startswith=to_search_form(query)
+    text = query or ""
+    return query_set.auto_query(text) | query_set.filter(
+        head__startswith=to_search_form(text)
     )
