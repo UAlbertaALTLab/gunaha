bb92da7a06f262996090a473f11aaeaf4c8a5257
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-11 12:57:45 -0600

    change: index heads by definition

--- apps/morphodict/search_indexes.py
+++ apps/morphodict/search_indexes.py
@@ -10,11 +10,12 @@ from haystack import indexes  # type: ignore
 from .models import Definition, Head
 
 
-class DefinitionIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, model_attr="text")
+class HeadIndex(indexes.SearchIndex, indexes.Indexable):
+    # See: ./apps/morphodict/templates/search/indexes/morphodict/head_text.txt
+    text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
-        return Definition
+        return Head
 
     def index_queryset(self, using=None):
-        return self.get_model().objects.all()
+        return self.get_model().objects.all().prefetch_related("definitions")
