9f36fc48030ce968da20d6255ce640de47444341
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-11 13:55:48 -0600

    change(gunaha): search using haystack

--- apps/gunaha/views.py
+++ apps/gunaha/views.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from haystack.inputs import AutoQuery  # type: ignore
+from haystack.query import SearchQuerySet  # type: ignore
 
 from apps.morphodict.models import Head
 
@@ -11,8 +13,8 @@ MAX_RESULTS_PER_PAGE = 30
 
 
 def index(request):
-    terms = search_entries(request.GET.get("q", None))
-    pages = Paginator(terms, MAX_RESULTS_PER_PAGE)
+    results = search_entries(request.GET.get("q", None))
+    pages = Paginator(results, MAX_RESULTS_PER_PAGE)
     # page page get page page get get get page get
     page = pages.get_page(request.GET.get("page", 1))
     return render(request, "gunaha/index.html", context={"page": page},)
@@ -22,9 +24,10 @@ def search_entries(query: Optional[str]):
     """
     Results are ALWAYS ordered!
     """
-    result_set = Head.objects.all()
 
-    if query is not None:
-        result_set = result_set.filter(text__istartswith=to_search_form(query))
+    query_set = SearchQuerySet().models(Head)
 
-    return result_set.prefetch_related("definitions")
+    if query is None:
+        return query_set
+
+    return query_set.filter(content=AutoQuery(query))
