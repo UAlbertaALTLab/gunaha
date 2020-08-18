ae03930c4e6f667e1766a83d7a422b00511965ea
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-18 09:24:54 -0600

    test(gunaha): results not found page

--- apps/gunaha/tests/test_search.py
+++ apps/gunaha/tests/test_search.py
@@ -10,5 +10,16 @@ import pytest
 
 @pytest.mark.django_db
 def test_search(client):
+    """
+    Test an ordinary search.
+    """
     res = client.get("/?q=tlicha")
     assert "dog" in res.content.decode("UTF-8")
+
+
+@pytest.mark.django_db
+def test_query_not_found(client):
+    res = client.get("?q=fhqwhgads")
+    assert res.status_code == 200
+    # TODO: more robust assertion
+    assert "No results for" in res.content.decode("UTF-8")
