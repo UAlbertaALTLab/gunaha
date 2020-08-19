df0624cf1e9a344fd0215c63ce5c609647108a71
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-18 20:00:48 -0600

    refactor(tests): extract helper: search_for_query()

--- apps/gunaha/tests/test_search.py
+++ apps/gunaha/tests/test_search.py
@@ -5,21 +5,42 @@
 Try some searches
 """
 
+from urllib.parse import urlencode
+
 import pytest
 
 
 @pytest.mark.django_db
-def test_search(client):
+def test_search(search_by_query):
     """
     Test an ordinary search.
     """
-    res = client.get("/?q=tlicha")
+    query = "tlicha"
+    res = search_by_query(query)
     assert "dog" in res.content.decode("UTF-8")
 
 
 @pytest.mark.django_db
-def test_query_not_found(client):
-    res = client.get("?q=fhqwhgads")
+def test_query_not_found(search_by_query):
+    non_word = "fhqwhgads"
+    res = search_by_query(non_word)
     assert res.status_code == 200
     # TODO: more robust assertion
     assert "No results for" in res.content.decode("UTF-8")
+
+
+@pytest.fixture
+def search_by_query(client):
+    """
+    Helper to make searching by query faster:
+
+        search_by_query(query: str, **additional_query_args) -> HttpResponse
+
+    """
+
+    def search(query, **kwargs):
+        kwargs.update(q=query)
+        res = client.get("/?" + urlencode(kwargs))
+        return res
+
+    return search
