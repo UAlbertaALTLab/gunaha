ab40147a33db2b948c395c3174fef7efe23ff0ac
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-18 20:51:11 -0600

    refactor(morphodict): move search_entries impl -> Head.objects.search()

--- apps/morphodict/managers.py
+++ apps/morphodict/managers.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+"""
+Customize how to query certain models.
+"""
+
 from typing import Optional
 
 from django.db import models
+from haystack.query import EmptySearchQuerySet, SearchQuerySet  # type: ignore
+
+from .search import to_search_form
 
 
 class HeadManager(models.Manager):
-    def search(self, query: Optional[str]):
-        # Import .search here to prevent an import cycle
-        from .search import search_entries
+    """
+    Customizes search for Head models. Adds the following convenience method:
 
-        return search_entries(query)
+
+        Head.objects.search(query) -> SearchQuerySet
+    """
+
+    def search(self, query: Optional[str]) -> SearchQuerySet:
+        """
+        Does a full text search based on the dictionary head.
+        """
+
+        from .models import Head
+
+        if query is None:
+            query_set = EmptySearchQuerySet()
+        else:
+            query_set = SearchQuerySet().models(Head)
+
+        text = query or ""
+        return query_set.auto_query(text) | query_set.filter(
+            head_simplified__startswith=to_search_form(text)
+        )
--- apps/morphodict/search.py
+++ apps/morphodict/search.py
@@ -8,26 +8,8 @@ Search utilties.
 from typing import Optional
 
 from django.conf import settings
-from haystack.query import EmptySearchQuerySet, SearchQuerySet  # type: ignore
 
-from .models import Head
 from .util import get_function
-
-
-def search_entries(query: Optional[str]):
-    """
-    Results are ALWAYS ordered!
-    """
-
-    if query is None:
-        query_set = EmptySearchQuerySet()
-    else:
-        query_set = SearchQuerySet().models(Head)
-
-    text = query or ""
-    return query_set.auto_query(text) | query_set.filter(
-        head_simplified__startswith=to_search_form(text)
-    )
 
 
 def to_search_form(query: str) -> str:
