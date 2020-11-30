e0028ee98d00ab0a637c3c71ef9ae2c53329da27
Eddie Antonio Santos <easantos@ualberta.ca>
2020-11-30 16:05:55 -0700

    feat: import recordings. Poorly.

--- apps/gunaha/import_recordings.py
+++ apps/gunaha/import_recordings.py
@@ -23,9 +23,14 @@ Then entry_id are Onespot IDs. Note that they may refer to duplicates, so check 
 they're in OnespotDuplicate first before associating it with a Head.
 """
 
+import io
 import logging
 import sqlite3
+import tempfile
 from typing import Optional
+
+from django.core.files import File
+from pydub import AudioSegment  # type: ignore
 
 from apps.gunaha.models import OnespotDuplicate, Recording
 from apps.morphodict.models import Head
@@ -51,6 +56,26 @@ def import_recordings(database_path: str):
                 logger.error("could not find head for %r", entry_id)
                 continue
         id_to_head[entry_id] = head
+
+    # cool, let's transcode audio!
+    with tempfile.TemporaryDirectory() as work_directory:
+        cursor = conn.execute("SELECT hash, entry_id, audio FROM recording")
+        for file_hash, entry_id, raw_wav_data in cursor:
+            if entry_id not in id_to_head:
+                continue
+
+            head = id_to_head[entry_id]
+            with io.BytesIO(raw_wav_data) as source_data:
+                segment = AudioSegment.from_wav(source_data)
+            # TODO: add metadata, because why not?
+            audio_file = segment.export(
+                f"{work_directory}/audio.m4a", format="ipod", codec="aac"
+            )
+            recording = Recording(
+                entry=head, compressed_audio=File(audio_file, name=f"{file_hash}.m4a"),
+            )
+            recording.save()
+            audio_file.close()
 
 
 def get_duplicate(entry_id: str) -> Optional[Head]:
