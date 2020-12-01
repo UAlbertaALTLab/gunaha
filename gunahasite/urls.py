fc874f15c5603bb3f277b4e9b9a05cf2b32c71f5
Eddie Antonio Santos <easantos@ualberta.ca>
2020-12-01 08:53:55 -0700

    feat: add rudimentary recordings support

--- gunahasite/urls.py
+++ gunahasite/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from apps import gunaha
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
-from apps import gunaha
 
 urlpatterns = [
     path("", include("apps.gunaha.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
